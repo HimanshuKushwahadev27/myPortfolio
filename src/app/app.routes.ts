71e5bd676ab4f0ec0bc5b1c8f76b110c1ebc4194
Eminence <himanshumaurya447756@gmail.com>
2026-03-21 06:45:09 +0000

    fixed page layout issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
-import { Routes } from '@angular/router';
+    import { Routes } from '@angular/router';
 
-export const routes: Routes = [
+    export const routes: Routes = [
 
- {path : 'skills', 
-  loadComponent: () =>   import('./skill-section/skill-section.component')
-      .then(m => m.SkillSectionComponent)
- },
-  {path : 'project', 
-  loadComponent: () =>   import('./project/project.component')
-      .then(m => m.ProjectComponent)
- },
-  {path : 'contact', 
-  loadComponent: () =>   import('./contacts/contacts.component')
-      .then(m => m.ContactsComponent)
- }
-];
+    {path : 'skills', 
+    loadComponent: () =>   import('./skill-section/skill-section.component')
+        .then(m => m.SkillSectionComponent)
+    },
+    {path : 'project', 
+    loadComponent: () =>   import('./project/project.component')
+        .then(m => m.ProjectComponent)
+    },
+    {path : 'contact', 
+    loadComponent: () =>   import('./contacts/contacts.component')
+        .then(m => m.ContactsComponent)
+    },
+    {path : '', 
+    loadComponent: () =>   import('./home-page/home-page.component')
+        .then(m => m.HomePageComponent)
+    }
+    ];
